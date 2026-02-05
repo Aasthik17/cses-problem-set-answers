e3995bb18d658bd439a83a073cb4fa0041e0e816
Aasthik17 <aasthik.work08@gmail.com>
2026-02-05 23:52:49 +0530

    Changes in Variable name

--- Sliding_Window_Problems/slidingwindowor.java
+++ Sliding_Window_Problems/slidingwindowor.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class slidingwindowxor{
+public class slidingwindowsum{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -13,18 +13,18 @@ public class slidingwindowxor{
         long b = Long.parseLong(st.nextToken());
         long c = Long.parseLong(st.nextToken());
         long res=0L;
-        long currXor=x;
+        long currSum=x;
         int start=0;
         long[] arr = new long[n];
         arr[0]=x;
         for(int i=0; i<n; i++){
             if(i>0){
                 arr[i] = (arr[i-1]*a+b)%c;
-                currXor ^= arr[i];
+                currSum = currSum | arr[i];
             }
             if((i-start+1)==k){
-                res = res ^ currXor;
-                currXor = currXor ^ arr[start];
+                res = res ^ currSum;
+                currSum = currSum | arr[start];
                 start++;
             }
         }
