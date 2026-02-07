3e61cff7068960be98def6d4787aa3edd9f663f6
Aasthik17 <aasthik.work08@gmail.com>
2026-02-07 23:54:44 +0530

    TLE solution for Sliding Window Minimum

---
+++ Sliding_Window_Problems/slidingwindowminimum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class slidingwindowminimum{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        long x = Long.parseLong(st.nextToken());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        long[] arr = new long[n];
+        arr[0]=x;
+        for(int i=1; i<n; i++){
+            arr[i]=(arr[i-1]*a+b)%c;
+        }
+        
+        long res = 0;
+        for(int i=0; i+k<=n; i++){
+            long min=Long.MAX_VALUE;
+            for(int j=i; j<i+k; j++){
+                min = Math.min(min, arr[j]);
+            }
+            res ^= min;
+        }
+        System.out.println(res);
+    }
+}
