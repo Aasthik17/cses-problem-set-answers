0dd7e48dd809617b35b09a144388224e7861e24c
Aasthik17 <aasthik.work08@gmail.com>
2026-05-09 12:58:20 +0530

    Working on Counting Bits

--- Bitwise_Operations/countingbits.java
+++ Bitwise_Operations/countingbits.java
@@ -2,8 +2,16 @@ import java.util.*;
 import java.io.*;
 
 public class countingbits{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        long res=0;
+        for(int i=1; i<=n; i<<=1){
+            long rep=i<<1;
+            res += (n/rep)*i;
+            long rem=n%rep;
+            res += Math.max(0L, rem-i+1);
+        }
+        System.out.println(res);
     }
 }
